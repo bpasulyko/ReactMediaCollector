afd7a24770bec1dae4ddd94d289aed7e764a101e
Brady <bpasulyko@gmail.com>
2016-08-27 10:32:42 -0600

    Added lodash

--- public/js/entry.js
+++ public/js/entry.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import _ from 'lodash';
 import Test from './content';
 
 import "../scss/style.scss";
@@ -6,4 +7,6 @@ import "../scss/style.scss";
 $(document).ready(() => {
     var thing = new Test({ text: 'thing1' });
     console.log(thing.get());
+    var arr = [1,2,3,4,5];
+    $('main').append(_.map(arr, function (n) { return n*2; }));
 });
